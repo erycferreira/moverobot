6d83134dbc38fa612576d4528735805766fc4c2e
Eryc Ferreira <eryc.ferreira@normatel.com.br>
2026-02-03 16:32:19 -0300

    :tada: feat: Commit inicial
    Criado base para projeto com spring boot com maven
    
    Adicionado dependencias devtools/spring web e validation

---
+++ src/main/java/com/erycferreira/moverobot/MoveRobotApplication.java
@@ -0,0 +1,13 @@
+package com.erycferreira.moverobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoveRobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoveRobotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erycferreira/moverobot/MoveRobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erycferreira.moverobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoveRobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
